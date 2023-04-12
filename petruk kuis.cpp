3c7de2aab7fc818be5c331d3f56fc10696777c78
IbnuMuhtar <112595037+IbnuMuhtar@users.noreply.github.com>
2023-04-12 21:39:46 +0700

    Add files via upload

--- petruk kuis.cpp
+++ petruk kuis.cpp
@@ -1,18 +1,57 @@
-#include <iostream>
-using namespace std;
+//#include <iostream>
+//using namespace std;
 
 //NPM : 2217051140
 
-int kali(int y,int x){
-	if (y == 0){
-		return 0;
-	}
-	else {
-		return x + kali (x , y-1);
-	}
-}
+//int kali(int y,int x){
+//	if (y == 0){
+//		return 0;
+//	}
+//	else {
+//		return x + kali (x , y-1);
+//	}
+//}
 
-int main(){
-	int y = 0, x = 4;
-	cout<<kali(y,x);
-}
+//int main(){
+//	int y = 0, x = 4;
+//	cout<<kali(y,x);
+//}
+
+#include<iostream>
+
+using namespace std;
+
+   int main() {
+      cout << "------------------------------------------------------------------------------------------------------------------------" << endl;
+      cout << "----------------------------------------------Mencari nilai Terbesar dan Terkecil---------------------------------------" << endl;
+      cout << "------------------------------------------------------------------------------------------------------------------------" << endl;
+
+       int angka, jumlah, max, min;
+
+       cout << "masukkan jumlah angka: ";
+       cin >> jumlah;
+       cout << endl;
+
+       for (int i = 1; i <= jumlah; i++) {
+
+       cout << "Masukkan Angka       : "; cin >> angka;
+
+       if ( i == 1 ) {
+             min = angka;
+             max = angka;
+            }
+            else if ( min > angka ) {
+               min = angka;
+            }
+            else if ( max < angka) {
+               max = angka;
+           }
+           else {}
+           }
+
+       cout << endl;
+       cout << "Nilai terkecil           : " <<  min << endl;
+       cout << "Nilai terbesar           : " <<  max << endl;
+
+    return 0;
+    }
